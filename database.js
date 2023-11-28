85af1350113a2ad0ecfc28047d90b08895cddf62
Malte <91071750+Loxiuz@users.noreply.github.com>
2023-11-28 14:59:45 +0100

    Update database.js

--- database.js
+++ database.js
@@ -5,10 +5,8 @@ import fs from "fs";
 const connection = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
-  port: process.env.MYSQL_PORT,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  multipleStatements: true,
 });
 
 if (process.env.MYSQL_CERT) {
