790b54d0f01b12de668b74f1e99971de3c06ed2f
Malte <91071750+Loxiuz@users.noreply.github.com>
2023-12-05 22:24:34 +0100

    update

--- backend.js
+++ backend.js
@@ -3,13 +3,16 @@
 import express from "express";
 import cors from "cors";
 import connection from "./database.js";
+import { resolve } from "path";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(process.env.MYSQL_PORT);
+app.listen(8080, () => {
+  console.log("App running...");
+});
 
 function errorResult(err, result, response) {
   if (err) {
@@ -29,4 +32,64 @@ app.get("/sponsors", async (req, res) => {
       errorResult(err, result, res);
     }
   );
+});
+app.post("/sponsors", async (req, res) => {
+  const reqBody = req.body;
+
+  try {
+    const selectResult = await new Promise((resolve, reject) => {
+      connection.query("SELECT sponsorId FROM sponsors", (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+
+    let randomId = 0;
+    const sponsorIds = new Set(
+      selectResult.map((id) => {
+        return id.sponsorId;
+      })
+    );
+
+    for (let i = 0; i < 200; i++) {
+      randomId = "K" + (Math.floor(Math.random() * 9000) + 1000);
+      if (!sponsorIds.has(randomId)) {
+        const insertResult = await new Promise((resolve, reject) => {
+          connection.query(
+            "INSERT INTO sponsors (sponsorId, sponsorName, sponsorEmail, privatErhverv, cprCvr, sponsorPhone, notes, reepayHandlePeriamma, foreningLetId, reepayHandleDonations, paymentPlatform, aktive) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            [
+              randomId,
+              reqBody.sponsorName,
+              reqBody.sponsorEmail,
+              reqBody.privatErhverv,
+              reqBody.cprCvr,
+              reqBody.sponsorPhone,
+              reqBody.notes,
+              reqBody.reepayHandlePeriamma,
+              reqBody.foreningLetId,
+              reqBody.reepayHandleDonations,
+              reqBody.paymentPlatform,
+              reqBody.aktive,
+            ],
+            (err, result) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve(result);
+              }
+            }
+          );
+        });
+        if (insertResult) {
+          console.log("Sponsor added!");
+          return res.status(200).json({ message: "Sponsor added!" });
+        }
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
 });
--- database.js
+++ database.js
@@ -1,6 +1,5 @@
 import mysql from "mysql2";
 import "dotenv/config";
-import fs from "fs";
 
 const connection = mysql.createConnection({
   host: process.env.MYSQL_HOST,
@@ -8,9 +7,5 @@ const connection = mysql.createConnection({
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
 });
-
-if (process.env.MYSQL_CERT) {
-  connection.ssl = { cs: fs.readFileSync("DigiCertGlobalRootCA.crt.pem") };
-}
 
 export default connection;
